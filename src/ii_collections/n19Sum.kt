a739e801dc52ef372fefb28a85c77105867796d4
expilu <expilu@gmail.com>
2018-01-12 11:42:10 +0000

    Solves ii 19

--- src/ii_collections/n19Sum.kt
+++ src/ii_collections/n19Sum.kt
@@ -8,5 +8,8 @@ fun example6() {
 fun Customer.getTotalOrderPrice(): Double {
     // Return the sum of prices of all products that a customer has ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    return orders.sumByDouble { it.products.sumByDouble { it.price } }
+
+    // Nota: mucho mejor en el brnach de resolutions
+    //return orders.flatMap { it.products }.sumByDouble { it.price }
 }
